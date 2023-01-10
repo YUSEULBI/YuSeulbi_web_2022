c7d400b95306dcea24553cb1ee2c8b87067d45cd
YUSEULBI <ha258ppy@gmail.com>
2023-01-10 19:46:49 +0900

    day08 js과제1

---
+++ frontend/public/js/1.기초문법/0.과제/과제1.js
@@ -0,0 +1,97 @@
+
+
+//과제1번
+
+
+/*is not defined
+	1. 정의 2. 이름오타 3. 저장새로고침 안함.
+*/
+
+/*
+*카멜 표기법: studentarray -> studentArray
+				onlogin -> onLogin 
+*/ 
+
+// 배열선언 위치 [1. 함수안에서 선언(함수실행마다 선언 - 누적 저장 안됨 , 초기화 ) 
+//             2. 함수 밖에서 선언 (스크립트 실행시 1번만 선언 - 누적)]
+ 
+ // 4. 배열 선언과 동시에 3개의 요소 저장
+let studentArray = [ '20230110' , '20230109' , '20230108' ]
+
+
+// 3. HTML : onclick="onlogin()"에 대한 함수 정의[만들기]
+function onlogin() { // 함수[onLogin] 시작점 
+	
+	console.log('onlogin 함수 실행;') // 확인용
+	
+	//5. input 마크업 js변수로 가져오기 [DOM객체]
+	 let sno = document.querySelector('.sno')
+	 // 5. <input> 마크업에 입력된 데이터 호출
+	 let snoValue = sno.value;
+		// * 확인
+		console.log( '5번체크' + snoValue )
+	// 6. 찾기 [만약에 배열내 입력한 값이 존재하면 / 못찾으면-1 찾으면=해당인덱스번호] 	
+	let sIndex = studentArray.indexOf(snoValue)
+		// * 확인
+		console.log('6번체크 : ' + sIndex )
+		
+	// 7. 논리 // 8. 출력 [ innerHTML 이용한 문구 출력 ]
+	if ( sIndex == -1 && snoValue.length==8 ){
+	/*{ alert('로그인실패')}*/
+	document.querySelector('.resultBox') .innerHTML = '없는 학번입니다.'
+	}
+	if (sIndex == 0 && snoValue.length==8) { /*else ( alert ('로그인성공') )*/
+		document.querySelector('.resultBox') .innerHTML = '로그인성공!' ;
+		document.querySelector('.sno').value = '';
+	}
+	
+	if ( snoValue.length!=8 )
+		{document.querySelector('.resultBox') .innerHTML = '8자리로 입력해주세요.' ;}
+	
+	if(sIndex == -1 && document.querySelector('.sno').value=="")
+		{document.querySelector('.resultBox') .innerHTML = '공백이면 학번을 입력해주세요~' }
+	
+	
+	 
+	
+} // 함수[onLogin] 끝
+
+
+//--------------------------2. 과제2 함수 ---------------------
+
+
+function onAdd() { // 함수 onAdd 시작점
+	
+	// 5. input 마크업 js 변수로 가져오기
+	let sno2 = document.querySelector('.sno2')
+	// 5. input 마크업에 입력된 데이터 호출
+	let sno2Value = sno2.value;
+	// * 5번 확인
+		console.log ('5번체크' + sno2Value) 
+	//6. 찾기
+	let sIndex2 = studentArray.indexOf(sno2Value)
+	//* 6번 확인
+		console.log ('6번체크' + sIndex2) //맞으면 0 틀리면-1
+		
+	//7. 논리 , 출력
+	
+	if ( sIndex2 == 0 ) { 
+	document.querySelector('.resultBox2') .innerHTML = '실패! 이미 존재하는 학번입니다!' 
+		 }
+	
+	if(sIndex2 == -1 && document.querySelector('.sno2').value=="")
+		{document.querySelector('.resultBox2') .innerHTML = '공백이면 학번을 입력해주세요~' } 
+	
+	if (sIndex2 == -1 && sno2Value.length!=8 && document.querySelector('.sno2').value!="" )
+		{document.querySelector('.resultBox2') .innerHTML = '8자리로 입력해주세요.' ;} 
+		
+	if ( sIndex2 == -1 && sno2Value.length==8 )
+	{	document.querySelector('.resultBox2') .innerHTML = '등록 성공'; studentArray.push(sno2Value) ; 
+		document.querySelector('.sno2') .value = ''; }
+		console.log (studentArray)
+	
+	// 공백 안내 resultBox2 에 출력
+	
+	
+	
+} // 함수 onAdd 끝
---
+++ frontend/public/js/1.기초문법/7.js_제어문.js
@@ -0,0 +1,110 @@
+/*
+
+	삼항연산자 : 조건 ? 참 : 거짓
+	제어문
+		IF : 만약에~ [ 경우의수 판단 ]
+		거짓이면 실행안함.
+		1. 형태 
+			1. if( 조건 ) [true]실행문;
+			2. if( 조건 ) {[true] 실행문1; 실행문2; 실행문3;  }
+				*실행문2개 이상일때 {} 이용한 묶음처리 하자
+			3. if(조건) {[true] 실행문; }
+				else{ [false]실행문;}
+				
+			4. if( 조건 ) { [true]실행문; }
+				else if( 조건2 ) { [true2]실행문; }
+				else if( 조건3 ) { [true3]실행문; }
+				else if( 조건4 ) { [true4]실행문; }
+				else{ [false]실행문; }
+				
+			5. 중첩
+				if( 조건1){
+					if(조건2){
+						if(조건3){
+							
+						}
+					}
+				} 
+*/		
+/*
+	; : 세미콜론 [명령어단위 마침표]	/ 	: 콜론
+	실행문
+	실행문; 실행문;
+	if(  ) [참]실행문
+	if(  ) [참]실행문  [if( ) 뒤에 ; 하지 말기]
+*/
+
+
+// 1. if 형태
+if(10 > 3) console.log('[참1] 10이 더 크다 ')
+if(10 > 20 ) console.log('[참2] 10이 더 크다 ')
+if(10 > 20 ); console.log('[참3] 10이 더 크다 ') // X 세미콜론으로 끊겨서 출력됨
+
+
+
+  
+
+// 2. if 형태2
+if(10 > 3 ) console.log('참1'); console.log('1.10이 더 크다');	   //  [X] 2번째 console if랑 상관없이 무조건 실행
+if(10>20) console.log('참2'); console.log('2. 10이 더 크다.');     // [X] 2번째 console if랑 상관없이 무조건 실행
+if(10>20) { console.log('참3'); console.log('3. 10이 더 크다.'); } // [O]
+
+
+// 3. 
+if(10>3){console.log('[참1] 10 더 크다')}
+else(console.log('[거짓1] 10 더 작다'))
+	//vs 삼항연산자 (코드 길어지면 가독성 떨어짐)
+10>3 ? console.log('참2') : console.log('[거짓2]')
+
+
+// 4. if 형태4
+if(10>=20){console.log ('[참1]10이 20보다 크다.')}	// 만약에 10>=20 이상이면
+else if(10>=20){console.log ('[참2]10이 15보다 크다.')}	// 아니면서 만약에 10>=15 이상이면
+else if(10>=10){console.log ('[참3]10이 10보다 이상.')}	// 아니면서 만약에 10>=10 이상이면
+else {console.log('[거짓] 10이 10미만이다.')}		// 그외
+	// vs 삼항연산자
+	
+
+/*
+	1. prompt 로 정수 1개 입력받아 90이상이면 '합격' 아니면 '탈락' console 출력
+	
+	2. prompt 로 성별 입력받아
+		'M'또는'm'또는'남'또는'남자' 일경우 '남자 이군요' console출력 
+		'W'또는'w'또는'여'또는'여자' 일경우 '여자 이군요' console출력
+		그외 '남자 : m,M,남,남자  여자:w,W,여,여자 로 입력해주세요' 라고   console출력
+	
+	3. prompt 로 점수 1개 입력받아 
+		90점 이상이면 'A등급'	80점 이상이면 'B등급' 70점 이상이면 'C등급' 그외 '탈락'
+		
+		if(조건) {[true] 실행문; }
+				else{ [false]실행문;}
+*/
+
+
+//문제1번
+
+let value1 = Number( prompt('점수') )
+if (value1 >= 90) { console.log('합격'); }
+else { console.log('탈락')}   
+//if(조건){참실행문;}
+//else(조건){거짓실행문;}
+// vs 삼항연산자 
+
+//문제2번
+let 성별 = prompt('성별 입력')
+ 
+if (성별== 'M' || 성별== 'm' || 성별== '남' || 성별== '남자') {console.log('남자 이군요'); }
+else if (성별== 'W' || 성별== 'w' || 성별== '여' || 성별== '여자') {console.log('여자 이군요'); }
+else {console.log('남자 : m,M,남,남자 / 여자:w,W,여,여자 로 입력해주세요') }
+
+/*else if( 조건4 ) { [true4]실행문; }
+				else{ [false]실행문; }*/
+
+
+//문제3번
+let value2 = Number( prompt('점수 입력') )
+if (value2>=90){console.log('A등급');}
+else if (value2>=80){console.log('B등급');} 
+else if (value2>=70){console.log('C등급');}
+else {console.log('탈락')}
+
