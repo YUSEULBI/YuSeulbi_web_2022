106a8752210d8e740713d715863481b463fef89b
YUSEULBI <ha258ppy@gmail.com>
2023-01-14 13:13:27 +0900

    과제4

--- frontend/public/js/0.과제/과제4/과제4.js
+++ frontend/public/js/0.과제/과제4/과제4.js
@@ -21,17 +21,11 @@ function newClick( ){
 	
 	let newBook =  document.querySelector('.newBookinput').value
 	
-	if( newBook.length>4 ){
-			if( newBook.length<11 ){
-		
-				도서목록.push(newBook)
-			 
-			}else{ alert('5~10자 사이의 도서명을 입력하세요.')
-			
-			}
-	}else{
-		alert('5~10자 사이의 도서명을 입력하세요.')
-	}
+		if( newBook.length>4 && newBook.length<11 ){
+			도서목록.push(newBook)
+		}else{
+			alert('5~10자 사이의 도서명을 입력하세요.')
+		}
 	
 	
 	
@@ -90,17 +84,26 @@ function 고객테이블(){
 	let bookTable2 = ` <tr> <th>번호</th> <th>도서명</th> <th>도서대여여부</th> <th>비고</th> </tr> `
 	
 	for(let i = 0 ; i < 도서목록.length ; i++  ){
-		bookTable2 += `<tr> 
-						<th> ${i+1} </th> 
-						<th> ${ 도서목록[i]} </th> 
-						<th> <div class="checkOut"></div> </th> 
-						<th> <button class="대여버튼"  >대여</button><button class="반납버튼" >반납</button> </th> 
-					</tr>`
-				
+		for ( let j = 0 ; j < 대여목록 ; j++ ){
+			if ( 도서목록[i] == 대여목록[j]){
+				bookTable2 += `<tr> 
+								<th> ${i+1} </th> 
+								<th> ${ 도서목록[i]} </th> 
+								<th> O </th> 
+								<th> <button class="반납버튼" >반납</button> </th> 
+							</tr>`
+			} else {
+				bookTable2 += `<tr> 
+								<th> ${i+1} </th> 
+								<th> ${ 도서목록[i]} </th> 
+								<th> X </th> 
+								<th> <button class="대여버튼"  >대여</button> </th> 
+							</tr>`
+			}
+		}
 	}
 	document.querySelector('.bookList').innerHTML = bookTable2
-	document.querySelector('.대여버튼').style.display = 'none'
-	document.querySelector('.반납버튼').style.display = 'none'
+	
 	
 }
 
