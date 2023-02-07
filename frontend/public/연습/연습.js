60956fcace468bf4ef7485c2a26e78e6f2b70fbd
YUSEULBI <ha258ppy@gmail.com>
2023-02-07 22:47:11 +0900

    영화 좌석선택 연습

--- frontend/public/연습/연습.js
+++ frontend/public/연습/연습.js
@@ -12,46 +12,54 @@ function seatPrint(){
 	
 	let html = ''
 	for ( let a = 0 ; a <= 6 ; a++ ){
+		let 알파벳 = abc[a]
+		
 		for ( let i = 1 ; i <= 15 ; i++ ){
-			html += `<button type="button" class="seatNum" onclick="seatSelect(${a},${i})">${abc[a]+i}</button>`
+			// 선택한 시트번호 구하기
+			
+			seat = (알파벳 + i)
+			html += `<button type="button" class="seatNum${seat} seatNum" onclick="seatSelect(${a},${i})">${seat}</button>`
 		}
 		html += '<br>'
 	}
-	/*console.log(html)*/
+	
 	document.querySelector('.seat').innerHTML = html
 	
 }
 
 
 // 좌석선택시 함수실행
-function seatSelect( a , i ){
-	let html = ''
+function seatSelect(a,i){
 	
-	// 선택한 시트번호 구하기
-	a = abc[a]
-	console.log( a )
-	console.log( i )
-	seat = (a + i)
+	let 알파벳 = abc[a]
+	seat = (알파벳 + i)
 	
 	
-	
-	//if ( selectSeat.indexOf(selectSeat[0]) >= 0 ){ html += ' , '}
-	
-	if ( selectSeat.indexOf(seat) > 0 ){
+	// 동일한 좌석번호가 배열에 없으면
+	if ( selectSeat.indexOf(seat) < 0 ){
+		console.log ( selectSeat.indexOf(seat) )
 		selectSeat.push(seat)
 		console.log(selectSeat)
-			
-	}
-	else if ( selectSeat.indexOf(seat) >= 0 ){
 		
+		// 선택한 좌석에 클래스 추가
+		document.querySelector(`.seatNum${seat}`).classList.add('selectbox')
+	
+	// 동일한 좌석번호가 배열에 있으면 배열에서 삭제
+	} else if ( selectSeat.indexOf(seat) >= 0  ){
 		selectSeat.splice( selectSeat.indexOf(seat) , 1 )
-		console.log(selectSeat)
+		console.log ( selectSeat)
 		
+		// 선택한 좌석에 클래스 삭제
+		document.querySelector(`.seatNum${seat}`).classList.remove('selectbox')
 	}
 	
-	for ( let s = 0 ; s < selectSeat.length ; s++  ){
-			selectSeat[s]
-	}
+		
+			
 	
-	document.querySelector('.seatNumber').innerHTML += html
+
+	
+	
+	
+	
+	document.querySelector('.seatNumber').innerHTML = selectSeat
 }
