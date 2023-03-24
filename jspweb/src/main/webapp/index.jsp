f1759dc34497d04a7bb956cb1834a56e51f4796a
504 <504@DESKTOP-VVQ86L0>
2023-03-24 11:25:09 +0900

    day57 마커선택시 사이드바 제품목록 제품상세 출력

--- jspweb/src/main/webapp/index.jsp
+++ jspweb/src/main/webapp/index.jsp
@@ -1,21 +1,36 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html style="height: 100%">
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
-<body>
+<body style="height: 100%"> <!-- 가로/세로 사이즈 생략시 auto( 내용물크기만큼 ) -->
 	<%@ include file="/header.jsp" %> <!-- (프로젝트명 생략,webapp이하생략)절대경로 -->
 	
 	
+	<!-- 검색창 구역 -->
+	<div style="position: fixed; left: 10px; top: 40%; z-index : 999; 
+			width: 100px; height: 100px; background-color: white;">
+		검색창
+	</div>
 	
-	<div id="map" style="width:100%;height:700px;"></div>
+	<!-- div -->
+	<div style="display: flex; width: 100%; height: 100%">
+	
+		<!-- 지도 -->
+		<div id="map" style="width:80%;height:80%;"></div>
+		
+		<!-- 사이드바 -->
+		<div class="productlistbox" style="width: 20%">
+			
+		</div>
+	</div>
 	
 	<%@include file="/footer.jsp" %>
 
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=4737aaadbf0b5529b4a298b1282498ea&libraries=clusterer"></script>
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=4737aaadbf0b5529b4a298b1282498ea&libraries=services,clusterer,drawing"></script>
 	
 	<script src="/jspweb/js/index.js" type="text/javascript"></script>
 </body>
--- jspweb/src/main/webapp/js/index.js
+++ jspweb/src/main/webapp/js/index.js
@@ -5,13 +5,28 @@ console.log('index js실행')
 
 
  
-$.ajax({
-	url : "/jspweb/product/info" ,
-	method : "get" ,
-	success : (r)=>{
-		console.log(r)
-	}
-})
+//$.ajax({
+//	url : "/jspweb/product/info" ,
+//	method : "get" ,
+//	success : (r)=>{
+//		console.log(r)
+//	}
+//})
+let productlist = null;
+ function productlistprint( ){
+	 let html = '<h3>제품목록페이지</h3>'
+		productlist.forEach( (p)=>{
+			html += `<div>
+						<span> ${p.pname} </span>
+						<span> ${p.pcomment} </span>
+						<span> ${p.pprice} </span>
+						<span> ${p.pstate} </span>
+						<span> ${p.pview} </span>
+						<span> ${p.pdate} </span>
+					</div>`
+		})
+		document.querySelector('.productlistbox').innerHTML = html;
+ }
  
 
  var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
@@ -28,16 +43,52 @@ $.ajax({
  
     // 데이터를 가져오기 위해 jQuery를 사용합니다
     // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
+    // $.ajax
     $.get("/jspweb/product/info", function(data) {
+		//---------------사이드바------------------------
+		productlist = data;	// 제품목록 결과를 전역변수에 담기
+		productlistprint();
+		
+		//---------------------마커 ---------------------------------
+		
         // 데이터에서 좌표 값을 가지고 마커를 표시합니다
         // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
+        
+        // map함수는 return된 값을 배열에 대입 vs forEach() return X
+        // map( (인덱스,반복객체명)=>{})
+        // forEach( (반복객체명,인덱스)=>{})
         var markers = $(data).map(function(i, position) {
 			console.log(position)
-            return new kakao.maps.Marker({
+			
+			// 마커에 추가코드 작성하기 위해 벼
+            let marker = new kakao.maps.Marker({
                 position : new kakao.maps.LatLng(position.plat, position.plng)
             });
+            
+              // 마커에 클릭이벤트를 등록합니다
+			kakao.maps.event.addListener(marker, 'click', function() {
+			      console.log(position.pno + "제품 클릭")
+			      let html = `
+			      			<button onclick="productlistprint()" > <== </button>
+			      			<h3>제품상세페이지</h3>
+			      			`  
+			      html += `<div>
+						<span> ${position.pname} </span>
+						<span> ${position.pcomment} </span>
+						<span> ${position.pprice} </span>
+						<span> ${position.pstate} </span>
+						<span> ${position.pview} </span>
+						<span> ${position.pdate} </span>
+						<span> <button type="button" > ♡ </button> </span>
+					</div>`
+				document.querySelector('.productlistbox').innerHTML = html;
+			});
+			
+			return marker;
         });
 
         // 클러스터러에 마커들을 추가합니다
         clusterer.addMarkers(markers);
+        
+        //-------------마커
     });
